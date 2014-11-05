8651921113252146094a7bf27dc508133d50af3b
Ashis Poddar <apoddar@apoddar-mbp.attlocal.net>
2014-11-05 10:45:34 -0800

    updated products

--- products.js
+++ products.js
@@ -4,39 +4,42 @@ var express = require('express');
 
 var app = express();
 
-app.get('/scrape',function(req,res){
-	
-//	res.send('hello world');
-	request('http://www.shutterfly.com/sitesearch/iphone?sflyPage=1', function (error, response, html) {
-  	if (!error && response.statusCode == 200) {
-    		//console.log(html);
-		var products = [];
-    		var $ = cheerio.load(html);
-    		$('span.cat_item_price').each(function(i, element){
-        		var a = $(this).prev();
-        		var name = a.text();
-        		name = name.replace(/[\n\r]/g, '').trim();
+app.get('/products',function(req,res){
 
-        		//var url = a.attr('href');
-        		//var url = a.html();
-        		//var url = a.contents();
-        		var url =  a.children('.thumbName').children().attr('href');;
-        		var pricing = $(this).text();
-        		pricing = pricing.replace(/[\n\r\t]/g,'').trim();
+    var url = 'http://www.shutterfly.com/photo-gifts/photo-mugs';
+    //var url = 'http://www.shutterfly.com/sitesearch/iphone?sflyPage=1';
+	request(url, function (error, response, html) {
+        
+        if (!error && response.statusCode == 200) {
+            var products = [];
+            var $ = cheerio.load(html);
+            $('span.cat_item_price').each(function(i, element){
+                var a = $(this).prev();
+                var name = a.text();
+                name = name.replace(/[\n\r\t]/g, '').trim();
 
-        		var product = {
-                		name: name,
-                		pricing:pricing,
-                		url:url
-        		};
-        		console.log(product);
-			products.push(product);
-    		});
-		res.send(products);
-  	}
-});
+                var url =  a.children('.thumbName').children().attr('href');;
+                var pricing = $(this).text();
+                pricing = pricing.replace(/[\n\r]/g,'').trim().split('\t');
+                var fromPrice;
+                for(i = 0; i< pricing.length; i++){
+                    price = pricing[i];
+                    if(price != undefined && price.length > 0 && price.charAt(0) == '$'){
+                        fromPrice = price;
+                    }
+                }
+                var product = {
+                        name: name,
+                        fromPrice :fromPrice,
+                        url:url
+                };
+                console.log(product);
+                products.push(product);
+            });
+        res.send(products);
+        }
+    });
 }); //end appget
-
 app.listen(3000);
 
 
